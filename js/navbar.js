661244d81f31e2b8662951a197658bd830bed942
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-05-30 17:37:09 +0200

    feat: add scroll-indicator in navbar and its styles

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,10 @@
 
+window.addEventListener("scroll", (event) => {
+  const scrollPercentage = window.scrollY / (document.body.scrollHeight - window.innerHeight) * 100
+  document.getElementById("navbar-scroll-indicator").style.width = `${scrollPercentage}%`;
+
+})
+
 document.getElementById("navbar-menu-button")
   .addEventListener("click", () => {
     document.getElementById("navbar-menu").classList.toggle("show-mobile-menu");
