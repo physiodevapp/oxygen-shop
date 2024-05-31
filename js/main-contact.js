ee334a1a150b07969db47b859b59e9a0d1dd64e9
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-05-31 10:15:51 +0200

    fix: submit eventListener

--- js/main-contact.js
+++ js/main-contact.js
@@ -75,16 +75,17 @@ function sendFormDataObject(dataObject) {
   }
 
 
-document.getElementById("contact-form").onsubmit = handleForm;
+document.getElementById("contact-form").addEventListener("submit", (event) => {
 
-function handleForm(event) {
   event.preventDefault();
-
+  
   const dataObject = formEventToObject(event);
-
+  
   const canSendData = isValidFormObject(dataObject);
-
+  
   if (canSendData) {
     sendFormDataObject(dataObject);
   }
-}
+
+});
+
