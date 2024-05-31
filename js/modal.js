e5989fd097411c405b596d9230ff65242d9914a7
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-05-31 13:47:38 +0200

    merge: resolve conflicts

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,27 @@
-
 const modal = document.getElementById("modal");
+const closeButton = document.getElementById("modal-close-button");
+let isModalShown = false;
 
 const showModal = (basedOn) => {
-  const currentScrollValue =  scrollPercentage();
+  const currentScrollValue = scrollPercentage();
 
   switch (basedOn) {
     case "time":
       const modalTimeout = setTimeout(() => {
-    
         clearTimeout(modalTimeout);
-    
+
         modal.classList.add("show-modal");
-    
       }, 5000);
-      
+
+      isModalShown = true;
+
       break;
-  
+
     case "scroll":
       if (currentScrollValue > 25) {
         modal.classList.add("show-modal");
+
+        isModalShown = true;
       }
 
       break;
@@ -26,7 +29,18 @@ const showModal = (basedOn) => {
     default:
       break;
   }
+};
 
+const hideModal = () => {
+  modal.classList.remove("show-modal");
 
+  const modalTimeout = setTimeout(() => {
+    clearTimeout(modalTimeout);
 
-}
+    modal.classList.add("display-none");
+  }, 200);
+};
+
+modal.addEventListener("click", hideModal);
+
+closeButton.addEventListener("click", hideModal);
